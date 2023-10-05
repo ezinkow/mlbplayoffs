ca3af845d394a8af3c61ea16b8e26c256eb5d343
Elan Zinkow <ezinkow@gmail.com>
2023-10-05 12:48:35 -0500

    a

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,15 +12,15 @@ export default function Home() {
             <div className='container'>
                 <br></br>
                 <br></br>
-                <a href='https://docs.google.com/spreadsheets/d/1mkjNASTQEeia3hUVVCvPMNRIMtDPvR9eqrbCCZjpnQI/' target="_blank"><h3>Check out the google doc for your picks results</h3></a>
+                <a href='https://docs.google.com/spreadsheets/d/1E3sHKvva-77XwQ8t9RV_aVt_B-ex0SomWSvSF5LsSmE/' target="_blank"><h3>Check out the google doc for your picks results</h3></a>
                 <br></br>
                 <br></br>
                 <Link to="/picks"><h3>Current Round Picks</h3></Link>
                 <br></br>
                 <br></br>
                 
-                <a href='https://www.nba.com/games' target="_blank"><h5>Scores</h5></a>
-                <a href='https://www.nba.com/playoffs/2023' target="_blank"><h5>Playoff Picture</h5></a>
+                <a href='https://www.mlb.com/scores' target="_blank"><h5>Scores</h5></a>
+                <a href='https://www.mlb.com/news/2023-mlb-playoff-and-world-series-schedule' target="_blank"><h5>Playoff Picture</h5></a>
             </div>
         </div>
     )
