7507d60437675374fa8a3c2c6c737070461570de
Elan Zinkow <ezinkow@gmail.com>
2023-10-06 09:12:08 -0500

    rd 2

--- client/src/components/Picks.js
+++ client/src/components/Picks.js
@@ -19,7 +19,7 @@ export default function PicksRound() {
     const [pointsTotal, setPointsTotal] = useState('')
     const [gamesTotal, setGamesTotal] = useState('')
 
-    const round = '4'
+    const round = '2'
 
     const customStyles = {
         content: {
