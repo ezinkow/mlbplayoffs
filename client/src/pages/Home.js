a98e7efae07ecc5da5e9800e8b5e7d6d878c746b
Elan Zinkow <ezinkow@gmail.com>
2024-09-30 22:16:44 -0500

    homepage update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,16 +11,12 @@ export default function Home() {
             <Navbar />
             <div className='container'>
                 <br></br>
-                <br></br>
-                <a href='https://docs.google.com/spreadsheets/d/1E3sHKvva-77XwQ8t9RV_aVt_B-ex0SomWSvSF5LsSmE/' target="_blank"><h3>Check out the google doc for your picks results</h3></a>
-                <br></br>
-                <br></br>
                 <Link to="/picks"><h3>Current Round Picks</h3></Link>
                 <br></br>
                 <br></br>
                 
                 <a href='https://www.mlb.com/scores' target="_blank"><h5>Scores</h5></a>
-                <a href='https://www.mlb.com/news/2023-mlb-playoff-and-world-series-schedule' target="_blank"><h5>Playoff Picture</h5></a>
+                <a href='https://www.mlb.com/postseason' target="_blank"><h5>Playoff Picture</h5></a>
             </div>
         </div>
     )
